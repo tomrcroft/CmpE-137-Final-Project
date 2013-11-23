168ee5ffc8589cd737a58d20de7fbe53dfe746a2
Alex <hehahohee@gmail.com>
2013-11-22 16:19:37 -0800

    Set up Web Pages
    Login.aspx - Start page, will set Session["user"] on successful login,
    has link to register. Redirects to ListPOIs on successful log in.
    
    Register.aspx - Creates user account. sets Session["user"] on successful
    register. Redirects to ListPOIs on successful register.
    
    ListPOIs.aspx - list places of interest. not accessible unless
    Session["user"] is set (and a real user?). each listed poi is a link to
    POIdetails.
    
    POIdetails.aspx - not accessible unless Session["user"] is set. displays
    details and reviews. May contain a form to make a review (should make
    review be a seperate page?)
    
    UserProfile.aspx displays information about users. lists favorite
    places. adding features here may lead to changes in Users table schema.

---
+++ Introduction_WebApplication1/Introduction_WebApplication1/Login.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Introduction_WebApplication1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session["test"] = TextBox1.Text;
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
