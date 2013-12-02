ed62e36ab220cca110e9f69172b97f66415777c6
talandaw <talandaw@gmail.com>
2013-12-01 18:28:23 -0800

    up to date userprofile page/don't try to run

--- Introduction_WebApplication1/Introduction_WebApplication1/UserProfile.aspx.cs
+++ Introduction_WebApplication1/Introduction_WebApplication1/UserProfile.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace Introduction_WebApplication1
 {
@@ -12,6 +13,43 @@ namespace Introduction_WebApplication1
         protected void Page_Load(object sender, EventArgs e)
         {
             Label1.Text = Session["Username"].ToString();
+
+        }
+
+        protected void Update_Click(object sender, EventArgs e)
+        {
+             //   SqlConnection myConnection = new SqlConnection("user id=xaigamer;" +
+                                     "server=localhost;" +
+                                     "Trusted_Connection=yes;" +
+             //                        "database=FinalProject; " +
+                                     "connection timeout=30");
+                try
+                {
+                    myConnection.Open();
+                }
+                finally
+                {
+
+                }
+
+                //SqlCommand myCommand = new SqlCommand("UPDATE Users SET Name=' WHERE Username ='"+Session["Username"].ToString()+"';", myConnection);
+                try
+                {
+                    myCommand.ExecuteNonQuery();
+                    //TextBox4.Text = "User registered";
+                    TextBox1.Text = "Profile Updated!";
+ 
+
+                }
+                catch (SqlException sqlerror)
+                {
+                    TextBox4.Text = "Username is already taken";
+                }
+            
+            else
+            {
+                TextBox4.Text = "Your Passwords do not match.";
+            }
         }
 
         
